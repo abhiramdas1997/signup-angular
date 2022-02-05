14a1680893f3cc1bc98f41e0137a35b49e117f36
Abhiram Das <abhiramdas2525@gmail.com>
2022-02-06 01:02:00 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'signup-reactive';
+  registerForm = new FormGroup({});
+  submitted:boolean=false;
+
+  constructor (private formBuilder : FormBuilder){}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName : ['',Validators.required,Validators.minLength(2)],
+      lastName : ['',Validators.required],
+      email : ['',Validators.required],
+      password : ['',Validators.required],
+      confirmPassword : ['',Validators.required],
+      acceptTandC : [false,Validators.requiredTrue]
+    })      
+  }
+
+  onSubmit (){
+    this.submitted=true;
+    if(this.registerForm.invalid){
+      return;
+    }
+    console.table(this.registerForm.value);
+    console.table(this.registerForm);
+
+    alert("Submitted Successfully\n" + JSON.stringify(this.registerForm.value));
+  }
+
+  onReset (){
+    this.submitted=false;
+    this.registerForm.reset();
+  }
+
+  
+}
